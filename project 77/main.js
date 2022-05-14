fc3b5ac7c69757570b993d1c7e07d0d4c2438577
VIHAAN <93201101+v-coding22@users.noreply.github.com>
2022-05-14 20:58:18 +0530

    Add files via upload

---
+++ project 77/main.js
@@ -0,0 +1,27 @@
+
+name1 = "Vihaan Desai";
+console.log(name1);
+
+project_name = "MY FIRST CONSOLE PROJECT";
+console.log(project_name);
+
+name2 = "Max Verstappen";
+console.log(name2);
+
+var a = 3;
+var b = 5;
+console.log(a * b);
+
+var a = 3;
+var b = 2;
+console.log(a + b);
+
+var a = 100;
+var b = 2;
+console.log(a/b);
+
+var a = 80;
+var b = 78;
+console.log(a-b);
+
+ 
